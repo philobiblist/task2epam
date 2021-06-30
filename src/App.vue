5f11bdf9d86135518bfc5adab27e269aaa423b33
philobiblist <95966716+philobiblist@users.noreply.github.com>
2021-06-30 22:21:17 +0000

    Look: Modif esthétique mineure

--- src/App.vue
+++ src/App.vue
@@ -22,3 +22,28 @@
         created() {
             this.$store.dispatch('initStocks');
         }
+    }
+</script>
+
+<style>
+    body {
+        padding: 30px;
+    }
+
+    .slide-enter-active {
+        animation: slide-in 200ms ease-out forwards;
+    }
+
+    .slide-leave-active {
+        animation: slide-out 200ms ease-out forwards;
+    }
+
+    @keyframes slide-in {
+        from {
+            transform: translateY(-30px);
+            opacity: 0;
+        }
+        to {
+            transform: translateY(0);
+            opacity: 1;
+        }
