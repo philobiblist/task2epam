d48b48080e2616e38d979d072ec35fe56dbb1177
philobiblist <95966716+philobiblist@users.noreply.github.com>
2021-07-04 10:46:38 +0000

    Style Updates

--- src/App.vue
+++ src/App.vue
@@ -47,3 +47,16 @@
             transform: translateY(0);
             opacity: 1;
         }
+    }
+
+    @keyframes slide-out {
+        from {
+            transform: translateY(0);
+            opacity: 1;
+        }
+        to {
+            transform: translateY(-30px);
+            opacity: 0;
+        }
+    }
+</style>
