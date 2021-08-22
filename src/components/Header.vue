bbb1be27a4a734a9e0059c4577d3305af4ffc392
philobiblist <95966716+philobiblist@users.noreply.github.com>
2021-08-22 19:33:05 +0000

    bilibili blacklist.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,4 +8,4 @@
             <div class="collapse navbar-collapse">
                 <ul class="nav navbar-nav">
                     <router-link to="/portfolio" activeClass="active" tag="li"><a>Portfolio</a></router-link>
-                    <router-link to="/stocks" activeClass="active"
+                    <router-link to="/stocks" activeClass="active" tag="li"><a>Stocks</a></router-l
