a0a8dd3d7b1a04b18df22d2f3b90ee74fb2661c5
philobiblist <95966716+philobiblist@users.noreply.github.com>
2022-11-06 05:27:48 +0000

    Update main.po (POEditor.com)

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,7 @@
 <template>
     <div>
-        <app-stock v-for="stock in stocks" :stock="stock" :key="stock.id"></app-sto
+        <app-stock v-for="stock in stocks" :stock="stock" :key="stock.id"></app-stock>
+    </div>
+</template>
+
+<scri
