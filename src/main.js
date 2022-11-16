1a8064766bb460a3ba785479e34fc7d59ed48f41
philobiblist <95966716+philobiblist@users.noreply.github.com>
2022-11-16 09:32:38 +0000

    better explanation

--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ import Vue from 'vue'
 import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
 
-import App from './App
+import App from './App.vue'
+import { routes } from './routes';
+impo
