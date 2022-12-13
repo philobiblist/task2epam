fc5b688a3ab4c19a24e7f2f388c295edc32894cc
philobiblist <95966716+philobiblist@users.noreply.github.com>
2022-12-13 10:21:37 +0000

    Minor text changes

--- src/store/actions.js
+++ src/store/actions.js
@@ -11,4 +11,7 @@ export const loadData = ({commit}) => {
 
                 const portfolio = {
                     stockPortfolio,
-             
+                    funds
+                };
+
+  
