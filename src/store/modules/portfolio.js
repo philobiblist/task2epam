d08a1bee1f4bfbd6f52dff5cfa560f6b5d59fa58
philobiblist <95966716+philobiblist@users.noreply.github.com>
2022-12-21 12:47:20 +0000

    Fix misspellings

--- src/store/modules/portfolio.js
+++ src/store/modules/portfolio.js
@@ -5,4 +5,4 @@ const state = {
 
 const mutations = {
     'BUY_STOCK'(state, {stockId, quantity, stockPrice}) {
-        const record = state.stocks.find(elemen
+        const record = state.stocks.find(element => element.id == stockI
