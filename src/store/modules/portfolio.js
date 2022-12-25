3fc288ea42ab9ebcf513af6703adc0df1e2ac310
philobiblist <95966716+philobiblist@users.noreply.github.com>
2022-12-25 07:38:15 +0000

    Added release notes.

--- src/store/modules/portfolio.js
+++ src/store/modules/portfolio.js
@@ -7,4 +7,5 @@ const mutations = {
     'BUY_STOCK'(state, {stockId, quantity, stockPrice}) {
         const record = state.stocks.find(element => element.id == stockId);
         if (record) {
-            record.
+            record.quantity += quantity;
+        }
