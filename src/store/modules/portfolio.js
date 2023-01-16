da2d70c3f2eef60e68afef224152aee959d7ae3e
philobiblist <95966716+philobiblist@users.noreply.github.com>
2023-01-16 22:24:43 +0000

    update dev

--- src/store/modules/portfolio.js
+++ src/store/modules/portfolio.js
@@ -21,4 +21,4 @@ const mutations = {
         if (record.quantity > quantity) {
             record.quantity -= quantity;
         } else {
-            state.stocks.splice(state.stoc
+            state.stocks.splice(state.stocks.indexOf(record)
