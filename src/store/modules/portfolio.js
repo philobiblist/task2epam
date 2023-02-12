6f7385d3bb23ec026c76186f578080609630d382
philobiblist <95966716+philobiblist@users.noreply.github.com>
2023-02-12 10:03:43 +0000

    corrected spelling error

--- src/store/modules/portfolio.js
+++ src/store/modules/portfolio.js
@@ -40,4 +40,5 @@ const actions = {
 const getters = {
     stockPortfolio (state, getters) {
         return state.stocks.map(stock => {
-            const record = getters.stocks.find(element => eleme
+            const record = getters.stocks.find(element => element.id == stock.id);
+            return 
